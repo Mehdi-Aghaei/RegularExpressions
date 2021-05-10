74dc80a0ed51bf260c48300950707bd2932b79f9
Cacti79 <mehdi1020@yahoo.com>
2021-05-10 17:37:34 +0200

    work with CSV file

--- Program.cs
+++ Program.cs
@@ -11,12 +11,14 @@ namespace RegularExp
             //string path = Directory.GetCurrentDirectory();
             string file_path = @"G:\Programming\CSharpPrograms\C#ConsoleProjects\RegularExp\data.txt";
             string Names = File.ReadAllText(file_path);
-
+            RegexElement myel = new RegexElement();
+            Console.WriteLine(myel.FindByElementName("Carbon"));
+            //Regex re = new Regex(@"a");
             //Regex re = new Regex(@"\S+");
-            //Regex re = new Regex(@".+"); every thinh and same format
+            //Regex re = new Regex(@".+"); every thing and same format
+            // we want phone number that just start with 0 or 8
             //Regex re = new Regex(@"[08]\d\d[*-]\d{3}[*-]\d{3,4}");
             // \d{3} exactly 3 digit
-            //Regex re = new Regex(@"[E]ri.+");
             //Regex re = new Regex(@"Mr\.?\s[a-z]\w*");
             // the char before that can have it  or no with using ? exp = https? ==> s is optional also we have optional group  exp= (www\.)?
             // we can create group with ();
@@ -24,33 +26,18 @@ namespace RegularExp
             // \w* zreo or more word charecter
             // email match
             //Regex re = new Regex(@"[A-Za-z0-9.-]+@[a-zA-Z-]+\.(com|net|edu)");
-            // we want phone number that just start with 0 or 8
+
             // for is not in range we use [^a-z]
             // in the brace we can write a specific charset that we want between pattern
             // we can say range with dash [1-7]number from one to seven
             // its important to know every brace represent 1 charecter [] == one chhar
-            string badString = "Here is a strig with ton of white space.";
-            string CleanedString = Regex.Replace(badString, "\\s+", " ");
-            string[] digits = Regex.Split(Names, @"\D+");
-            Regex re = new Regex(@"https?://(www\.)?(\w+)(\.\w+)");
+            //string badString = "Here is a strig with ton of white space.";
+            //string CleanedString = Regex.Replace(badString, "\\s+", " ");
+            //string[] digits = Regex.Split(Names, @"\D+");
+            //Regex re = new Regex(@"https?://(www\.)?(\w+)(\.\w+)");
             //Regex re = new Regex(@"[aeiou]");
             // vowel finder
             // url finder also we have grop 1,2,3 which is $1 $2 $3 
-            MatchCollection matchedNames = re.Matches(Names);
-            int a;
-            a = matchedNames.Count;
-            if (a > 0)
-            {
-                foreach (var item in matchedNames)
-                {
-                    Console.WriteLine(item);
-                }
-            }
-            else
-            {
-                Console.WriteLine("couldn't find any match");
-            }
-
         }
     }
 }
